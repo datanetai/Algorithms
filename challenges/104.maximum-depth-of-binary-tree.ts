c7e96dfc70c54614f55d00ff1eead46bfb067cb8
datanetai <ameerhamza0220@gmail.com>
2024-02-07 14:13:03 +0500

    Add solution for LeetCode problem 104: Maximum Depth of Binary Tree

---
+++ challenges/104.maximum-depth-of-binary-tree.ts
@@ -0,0 +1,75 @@
+/*
+ * @lc app=leetcode id=104 lang=typescript
+ *
+ * [104] Maximum Depth of Binary Tree
+ *
+ * https://leetcode.com/problems/maximum-depth-of-binary-tree/description/
+ *
+ * algorithms
+ * Easy (74.68%)
+ * Likes:    12161
+ * Dislikes: 203
+ * Total Accepted:    2.7M
+ * Total Submissions: 3.7M
+ * Testcase Example:  '[3,9,20,null,null,15,7]'
+ *
+ * Given the root of a binary tree, return its maximum depth.
+ * 
+ * A binary tree's maximum depth is the number of nodes along the longest path
+ * from the root node down to the farthest leaf node.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: root = [3,9,20,null,null,15,7]
+ * Output: 3
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: root = [1,null,2]
+ * Output: 2
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * The number of nodes in the tree is in the range [0, 10^4].
+ * -100 <= Node.val <= 100
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function maxDepth(root: TreeNode | null): number {
+    function recurse(node: TreeNode | null, depth: number): number {
+        if (node === null) {
+            return depth;
+        }
+        // If the node is a leaf node, return the current depth (improves runtime significantly)
+        if (node.left === null && node.right === null) {
+            return depth + 1;
+        }
+        return Math.max(recurse(node.left, depth + 1), recurse(node.right, depth + 1));
+    }
+    return recurse(root, 0);
+};
+// @lc code=end
+
