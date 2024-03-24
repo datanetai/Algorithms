c209197ac14d07fba269855654d908d28bc311bb
datanetai <ameerhamza0220@gmail.com>
2024-03-24 15:49:14 +0500

    Add SmallestInfiniteSet class implementation

---
+++ challenges/smallest_int.py
@@ -0,0 +1,56 @@
+# 2336. Smallest Number in Infinite Set
+# Medium
+# Topics
+# Companies
+# Hint
+# You have a set which contains all positive integers [1, 2, 3, 4, 5, ...].
+
+# Implement the SmallestInfiniteSet class:
+
+# SmallestInfiniteSet() Initializes the SmallestInfiniteSet object to contain all positive integers.
+# int popSmallest() Removes and returns the smallest integer contained in the infinite set.
+# void addBack(int num) Adds a positive integer num back into the infinite set, if it is not already in the infinite set.
+
+# Example 1:
+
+# Input
+# ["SmallestInfiniteSet", "addBack", "popSmallest", "popSmallest", "popSmallest", "addBack", "popSmallest", "popSmallest", "popSmallest"]
+# [[], [2], [], [], [], [1], [], [], []]
+# Output
+# [null, null, 1, 2, 3, null, 1, 4, 5]
+
+# Explanation
+# SmallestInfiniteSet smallestInfiniteSet = new SmallestInfiniteSet();
+# smallestInfiniteSet.addBack(2);    // 2 is already in the set, so no change is made.
+# smallestInfiniteSet.popSmallest(); // return 1, since 1 is the smallest number, and remove it from the set.
+# smallestInfiniteSet.popSmallest(); // return 2, and remove it from the set.
+# smallestInfiniteSet.popSmallest(); // return 3, and remove it from the set.
+# smallestInfiniteSet.addBack(1);    // 1 is added back to the set.
+# smallestInfiniteSet.popSmallest(); // return 1, since 1 was added back to the set and
+#                                    // is the smallest number, and remove it from the set.
+# smallestInfiniteSet.popSmallest(); // return 4, and remove it from the set.
+# smallestInfiniteSet.popSmallest(); // return 5, and remove it from the set.
+
+
+class SmallestInfiniteSet:
+
+    def __init__(self):
+        self.smallest = 1
+        self.used = set()  
+
+    def popSmallest(self) -> int:
+        while self.smallest in self.used:
+            self.smallest += 1
+        self.used.add(self.smallest)
+        return self.smallest
+
+    def addBack(self, num: int) -> None:
+        if num < self.smallest:
+            self.smallest = num  
+        self.used.discard(num) 
+
+
+# Your SmallestInfiniteSet object will be instantiated and called as such:
+# obj = SmallestInfiniteSet()
+# param_1 = obj.popSmallest()
+# obj.addBack(num)
